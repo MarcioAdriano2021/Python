2c25ba06013f56be905696d9c5bcb64e782b3950
Márcio Adriano <71799268+MarcioAdriano2021@users.noreply.github.com>
2021-01-19 17:30:58 -0300

    Delete Web/Django/igtiflixweb directory

--- Web/Django/igtiflixweb/genero/admin.py
+++
@@ -1,3 +0,0 @@
-from django.contrib import admin
-from genero.models import Genero
-admin.site.register(Genero)
--- Web/Django/igtiflixweb/genero/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from genero.models import Genero
-
-class GeneroForm(forms.ModelForm):
-
-    class Meta:
-        model = Genero
-        fields = '__all__'
